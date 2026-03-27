5b019f970c03ceb7b664f39a9b3638d1ecb546ed
inforcqb <fanjiawei080615@qq.com>
2026-03-27 18:26:37 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1869,6 +1869,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+	#ifdef CONFIG_KSU
+	    ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+    #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1879,9 +1882,6 @@ int do_execveat(int fd, struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-    #ifdef CONFIG_KSU
-	    ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-    #endif
 	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 
